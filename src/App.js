b76ca3b727d641325500f0b10f658ca9aca99366
Jeanne Viallard <viallard.jeanne@gmail.com>
2024-04-01 11:31:25 +0200

    Add load more jobs button and functionality

--- src/App.js
+++ src/App.js
@@ -8,20 +8,36 @@ import "./App.css";
 export const ThemeContext = createContext();
 
 export default function App() {
-  const [jobs, setJobs] = useState(data);
-
+  const [jobs, setJobs] = useState(data.slice(0, 12));
+  const [hasLoadedAllJobs, setHasLoadedAllJobs] = useState(false);
   const [theme, setTheme] = useState("light");
+
+  function loadMore() {
+    const newJobs = data.slice(12);
+
+    setJobs((prevJobs) => {
+      return [...prevJobs, ...newJobs];
+    });
+
+    setHasLoadedAllJobs(true);
+  }
 
   return (
     <div className={`container container--${theme}`}>
       <ThemeContext.Provider value={{ theme, setTheme }}>
         <Header />
-        <main>
+        <main className="jobs-page__main">
           <div className="jobs-container">
             {jobs.map((job) => {
               return <Job key={job.id} data={job} />;
             })}
           </div>
+
+          {hasLoadedAllJobs ? null : (
+            <button onClick={loadMore} className="jobs-page__btn">
+              Load more
+            </button>
+          )}
         </main>
       </ThemeContext.Provider>
     </div>
